edf10b1f8617864b9e2dd1374e3a7dc16eafd20f
Lloyd Rochester <lloyd.rochester@dataductus.com>
2018-09-27 06:37:27 -0600

    changes to make test easier

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-all: ex1.o psk31_dec.o
+all: ex1.o psk31_dec.o test.o
+
+test:
+	printf "03210010111311022130\n"  | ./psk31_dec.o # 0 errors
+	printf "03200010111311022130\n"  | ./psk31_dec.o # 1 errors
 
 %.o: %.c
 	cc -o $@ $<
--- psk31_dec.c
+++ psk31_dec.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <assert.h>
 
 #define K 3
 #define NUM_STATES 32
@@ -8,7 +10,9 @@
 #define INPUT_LENGTH 20
 
 // the received output states with I & Q
-unsigned int input[INPUT_LENGTH] = {0,3,2,1,0,0,1,0,1,1,1,3,1,1,0,2,2,1,3,0};
+
+void read_input(unsigned int *input, unsigned int *len);
+//unsigned int input[INPUT_LENGTH] = {0,3,2,1,0,0,1,0,1,1,1,3,1,1,0,2,2,1,3,0};
 
 /* for a given state this structure represents
  * the what the next state will be given we
@@ -92,40 +96,40 @@ unsigned int output_state_arr[NUM_STATES][2] =
   {2,1}  // state 31
 };
 
-unsigned int state_trans[NUM_STATES][32] =
+unsigned int state_trans[NUM_STATES][NUM_STATES] =
 {
-  { 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 1,
-  {255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 2,
-  {255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 3,
-  {255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 4,
-  {255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 5,
-  {255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 6,
-  {255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 7,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 8,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 9,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255}, // state 10,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255}, // state 11,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255}, // state 12,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255}, // state 13,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255}, // state 14,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255}, // state 15,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1}, // state 16,
-  { 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 17,
-  {255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 18,
-  {255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 19,
-  {255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 20,
-  {255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 21,
-  {255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 22,
-  {255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 23,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 24,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255,255,255}, // state 25,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255,255,255}, // state 26,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255,255,255}, // state 27,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255,255,255}, // state 28,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255,255,255}, // state 29,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255,255,255}, // state 30,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1,255,255}, // state 31,
-  {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255, 0, 1}, // state 32
+  {0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 0
+  {3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 1
+  {3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 2
+  {3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 3
+  {3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 4
+  {3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 5
+  {3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 6
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 7
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 8
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3}, // state 9
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3}, // state 10
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3}, // state 11
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3}, // state 12
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3}, // state 13
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3}, // state 14
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1}, // state 15
+  {0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 16
+  {3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 17
+  {3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 18
+  {3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 19
+  {3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 20
+  {3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 21
+  {3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 22
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 23
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // state 24
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3,3,3}, // state 25
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3,3,3}, // state 26
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3,3,3}, // state 27
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3,3,3}, // state 28
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3,3,3}, // state 29
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1,3,3}, // state 30
+  {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,1}  // state 31
 };
 
 /*
@@ -135,10 +139,7 @@ unsigned int state_trans[NUM_STATES][32] =
  */
 
 // the path metric for each state transition
-unsigned int acc_metric[NUM_STATES][INPUT_LENGTH];
-
-// the suvivor states at each time sample
-unsigned int surviv[NUM_STATES][INPUT_LENGTH];
+//unsigned int acc_metric[NUM_STATES][INPUT_LENGTH];
 
 // add-compare-select array for each state
 // this array is populated per time sample
@@ -149,128 +150,69 @@ unsigned int acs[NUM_STATES];
 // used to traceback through the acc_metric array to
 // determine which output states have the lowest
 // metric
-unsigned int traceback[INPUT_LENGTH];
+//unsigned int traceback[INPUT_LENGTH];
 
 // once the traceback array output states are found
 // we can use the state_trans to find the encoded
 // bits that were an input to the convolutional
 // encoder
-unsigned int decoded[INPUT_LENGTH];
+//unsigned int decoded[INPUT_LENGTH];
 
 unsigned int hamming_distance(unsigned int a, unsigned int b);
+
+void cnv_dec(unsigned int encoded[], unsigned int len, unsigned int *decoded[]);
 
 /*
  * functions for debugging
  */
-void print_survivors();
-void print_acc_metrics();
-void print_traceback();
-void print_decoded();
+void print_acc_metrics(unsigned int acc_metric[NUM_STATES][1024], int len);
+void print_traceback(unsigned int traceback[], int len);
+void print_decoded(unsigned int decoded[], int len);
 
 int main(int argc, char *argv[])
 {
+  unsigned int input[1024];
+  unsigned int len;
+  unsigned int *decoded;
 
-  for(int t=0;t<INPUT_LENGTH;t++)
-  {
-    for(int state=0;state<NUM_STATES;state++)
+  FILE* fdo = stdout;
+
+  read_input(input,&len);
+  cnv_dec(input,len,&decoded);
+  for(int i=0;i<len;i++) {
+    fprintf(fdo,"%d",decoded[i]);
+  }
+  free(decoded);
+}
+
+void read_input(unsigned int *input, unsigned int *len)
+{
+  int n = 0;
+  char buf[1];
+
+  while(read(0, buf, sizeof(buf))>0) {
+    switch(buf[0])
     {
-      int initval = t<4 ? 9 : 0;
-      acc_metric[state][t] = initval;
+      case '0':
+        input[n++] = 0;
+        break;
+      case '1':
+        input[n++] = 1;
+        break;
+      case '2':
+        input[n++] = 2;
+        break;
+      case '3':
+        input[n++] = 3;
+        break;
+      case '\n':
+        *len = n;
+        return;
+        break;
+      default:
+        break;
     }
   }
-
-  unsigned int d0,d1;
-  unsigned int ns0,ns1;
-  unsigned int os0,os1;
-  for(int t=0;t<INPUT_LENGTH;t++)
-  {
-    // TODO
-    for(int state=0;state<NUM_STATES;state++) acs[state] = 255;
-    // i = 0 we have 2 paths
-    // i = 1 we have 2*2 = 4 paths
-    // i = 2 we have 2*2*2 = 8 paths
-    // i = 3 we have 2^4 = 16 paths
-    // i = 4 we have 32 paths
-    // i = 5 we have 64 paths where best one so 32
-    for(int state=0;state<NUM_STATES;state++)
-    {
-      ns0 = next_state_arr[state][0];
-      ns1 = next_state_arr[state][1];
-
-      os0 = output_state_arr[ns0][0];
-      os1 = output_state_arr[ns1][1];
-
-      d0 = hamming_distance(os0,input[t]);
-      d1 = hamming_distance(os1,input[t]);
-
-      if(t==0 && state==0)
-      {
-        acc_metric[os0][t] = d0;
-        acc_metric[os1][t] = d1;
-
-        if(t<3) {
-          printf("dist state=%d os0=%d input=%d\n",state,os0,input[t]);
-          printf("dist state=%d os1=%d input=%d\n",state,os1,input[t]);
-        }
-      }
-      if(t>0)
-      {
-        unsigned int acc_metrics[2];
-        unsigned int prev_metric;
-        unsigned int surviv_state;
-        unsigned int nns0=99,nns1=99;
-        unsigned int w0=0,w1=0;
-
-        prev_metric = acc_metric[state][t-1];
-
-        acc_metrics[0] = d0+prev_metric;
-        acc_metrics[1] = d1+prev_metric;
-
-        if(acc_metrics[0] < acs[ns0]) {
-          acs[ns0] = acc_metrics[0];
-          w0 = 1;
-        }
-
-        if(acc_metrics[1] < acs[ns1]) {
-          acs[ns1] = acc_metrics[1];
-          w1 = 1;
-        }
-
-        acc_metric[ns0][t] = acs[ns0];
-        acc_metric[ns1][t] = acs[ns1];
-
-        if(t<3)
-        {
-          printf("t=%d w0=%d state=%d input=%d ns=%d %d+%d acc_metric=%d surviv_state=%d nns0=%d nns1=%d\n",t,w0,state,input[t],ns0,prev_metric,d0,acs[ns0],surviv_state,nns0,nns1);
-          printf("t=%d w1=%d state=%d input=%d ns=%d %d+%d acc_metric=%d surviv_state=%d nns0=%d nns1=%d\n",t,w1,state,input[t],ns1,prev_metric,d1,acs[ns1],surviv_state,nns0,nns1);
-        }
-      }
-    }
-  }
-
-  for(int t=0;t<INPUT_LENGTH;t++)
-  {
-    unsigned int min_metric = 255;
-    unsigned int min_state = 0;
-    for(int state=0;state<NUM_STATES;state++)
-    {
-      if(acc_metric[state][t] < min_metric) {
-        min_state = state;
-        min_metric = acc_metric[state][t];
-      }
-    }
-    traceback[t] = min_state;
-  }
-
-  for(int t=1;t<INPUT_LENGTH-1;t++)
-  {
-    decoded[t-1] = state_trans[traceback[t-1]][traceback[t]];
-  }
-
-  print_acc_metrics();
-  print_survivors();
-  print_traceback();
-  print_decoded();
 }
 
 unsigned int hamming_distance(unsigned int a, unsigned int b)
@@ -297,47 +239,137 @@ unsigned int hamming_distance(unsigned int a, unsigned int b)
   return distance;
 }
 
-void print_survivors()
+void cnv_dec(unsigned int encoded[], unsigned int len, unsigned int *decoded[])
 {
-  puts("surviving predecessors:");
-  for(int i=0;i<NUM_STATES+1;i++)
+  /*
+   * These arrays are populated as the decoder processes
+   * each time sample. They use the state arrays that are
+   * already initialized above
+   */
+
+  // the path metric for each state transition
+  unsigned int acc_metric[NUM_STATES][1024];
+
+  // used to traceback through the acc_metric array to
+  // determine which output states have the lowest
+  // metric
+  unsigned int *traceback;
+
+  // distance from next state given a 0 and 1
+  unsigned int d0,d1;
+
+  // next state given current state if next is 0 or 1
+  unsigned int ns0,ns1;
+
+  // output state if next is 0 or 1
+  unsigned int os0,os1;
+
+  assert(len<1024); // TODO
+
+  traceback = (unsigned int*) calloc(len,sizeof(int));
+  // TODO wny need array
+  unsigned int *_decoded = (unsigned int*) calloc(len,sizeof(int));
+  *decoded = _decoded;
+
+  for(int t=0;t<len;t++)
   {
-    for(int j=0;j<INPUT_LENGTH+1;j++)
+    for(int state=0;state<NUM_STATES;state++)
     {
-      switch(i)
+      int initval = t<4 ? 9 : 0;
+      acc_metric[state][t] = initval;
+    }
+  }
+
+  for(int t=0;t<len;t++)
+  {
+    // initialize ACS to anything smaller than 255 will be used
+    for(int state=0;state<NUM_STATES;state++) acs[state] = 255;
+    for(int state=0;state<NUM_STATES;state++)
+    {
+      ns0 = next_state_arr[state][0];
+      ns1 = next_state_arr[state][1];
+
+      os0 = output_state_arr[ns0][0];
+      os1 = output_state_arr[ns1][1];
+
+      d0 = hamming_distance(os0,encoded[t]);
+      d1 = hamming_distance(os1,encoded[t]);
+
+      // since the state machine starts at state 0 at time 0
+      // we will only compute for state transtions of 0
+      if(t==0 && state==0)
       {
-        case 0:
-          switch(j)
-          {
-            case 0:
-              printf("t=   ");
-              break;
-            default:
-              printf("%3d ",j-1);
-          }
-          break;
-        default:
-          switch(j)
-          {
-            case 0:
-              printf("%2d   ",i-1);
-              break;
-            default:
-              printf("%3d ",surviv[i-1][j-1]);
-          }
+        acc_metric[ns0][t] = d0;
+        acc_metric[ns1][t] = d1;
+      }
+      if(t>0)
+      {
+        unsigned int acc_metrics[2];
+        unsigned int prev_metric;
+
+        prev_metric = acc_metric[state][t-1];
+
+        acc_metrics[0] = d0+prev_metric;
+        acc_metrics[1] = d1+prev_metric;
+
+        if(acc_metrics[0] < acs[ns0]) {
+          acs[ns0] = acc_metrics[0];
+        }
+
+        if(acc_metrics[1] < acs[ns1]) {
+          acs[ns1] = acc_metrics[1];
+        }
+
+        acc_metric[ns0][t] = acs[ns0];
+        acc_metric[ns1][t] = acs[ns1];
       }
     }
-    puts("");
   }
-  puts("");
+
+  for(int t=0;t<len;t++)
+  {
+    unsigned int min_metric = 255;
+    unsigned int min_metric2 = 255;
+    unsigned int min_state = 0;
+    unsigned int min_state2 = 0;
+    for(int state=0;state<NUM_STATES;state++)
+    {
+      unsigned int metric = acc_metric[state][t];
+      if(metric < min_metric)
+      {
+        min_state = state;
+        min_metric = metric;
+      }
+      else if(min_metric < 255 && metric == min_metric)
+      {
+        min_state2 = state;
+        min_metric2 = metric;
+      }
+    }
+    if(min_state%2 == 0 && acc_metric[(min_state+1)%NUM_STATES][t] == min_metric) printf("%d",min_metric);
+    traceback[t] = min_state;
+    if(t>0)
+      _decoded[t-1] = state_trans[traceback[t-1]][traceback[t]];
+  }
+
+  /*for(int t=1;t<len-1;t++)
+  {
+    _decoded[t-1] = state_trans[traceback[t-1]][traceback[t]];
+  }*/
+
+  free(traceback);
+
+  print_acc_metrics(acc_metric,len);
+  print_traceback(traceback,len);
+  print_decoded(_decoded,len);
 }
 
-void print_acc_metrics()
+void print_acc_metrics(unsigned int acc_metric[NUM_STATES][1024], int len)
 {
   puts("accumulated metrics:");
   for(int i=0;i<NUM_STATES+1;i++)
   {
-    for(int j=0;j<INPUT_LENGTH+1;j++)
+    for(int j=0;j<len+1;j++)
     {
       switch(i)
       {
@@ -367,12 +399,12 @@ void print_acc_metrics()
   puts("");
 }
 
-void print_traceback()
+void print_traceback(unsigned int traceback[], int len)
 {
   puts("traceback:");
   for(int i=0;i<1+1;i++)
   {
-    for(int j=0;j<INPUT_LENGTH+1;j++)
+    for(int j=0;j<len+1;j++)
     {
       switch(i)
       {
@@ -402,12 +434,12 @@ void print_traceback()
   puts("");
 }
 
-void print_decoded()
+void print_decoded(unsigned int decoded[], int len)
 {
   puts("decoded:");
   for(int i=0;i<1+1;i++)
   {
-    for(int j=0;j<INPUT_LENGTH+1-1;j++)
+    for(int j=0;j<len+1-1;j++)
     {
       switch(i)
       {
